9c05fac14428a5e339fcad3f8bd4e1ccfd7b612e
lisandrofort <fortlisandro0@gmail.com>
2024-09-01 10:25:49 -0300

    Cambios "Dark Mode Btn"

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded",()=>{
     let body = document.querySelector("body");
     let DarkModeBtn = document.querySelector("#DarkModeBtn");
+    let DarkModeBtnImg = document.getElementById("DarkModeBtnImg");
     
     function toggleDarkMode() {
         if (body.classList.contains("dark-mode")) {
             body.classList.remove("dark-mode");
+            DarkModeBtnImg.classList.remove("DarkModeBtnMoon");
+            DarkModeBtnImg.classList.add("DarkModeBtnSun");
         }else{
             body.classList.add("dark-mode");
+            DarkModeBtnImg.classList.remove("DarkModeBtnSun");
+            DarkModeBtnImg.classList.add("DarkModeBtnMoon");
         }
     }
     
