8dfccdf58895cbe24018ab3dd04e64931e965994
lisandrofort <fortlisandro0@gmail.com>
2024-09-02 07:29:28 -0300

    Implementación del Esquema PNG

--- scripts/script.js
+++ scripts/script.js
@@ -6,12 +6,16 @@ document.addEventListener("DOMContentLoaded",()=>{
     function toggleDarkMode() {
         if (body.classList.contains("dark-mode")) {
             body.classList.remove("dark-mode");
+            DarkModeBtn.classList.add("bgMoon");
+            DarkModeBtn.classList.remove("bgSun");
             DarkModeBtnImg.classList.remove("DarkModeBtnMoon");
             DarkModeBtnImg.classList.add("DarkModeBtnSun");
         }else{
             body.classList.add("dark-mode");
             DarkModeBtnImg.classList.remove("DarkModeBtnSun");
             DarkModeBtnImg.classList.add("DarkModeBtnMoon");
+            DarkModeBtn.classList.add("bgSun");
+            DarkModeBtn.classList.remove("bgMoon");
         }
     }
     
